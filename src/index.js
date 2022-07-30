54f92fc98221253c1d4567ea6754ae10463e7e2f
Leo-Tampaare <105938764+Leo-Tampaare@users.noreply.github.com>
2022-07-30 22:57:00 +0300

    Showing current temperature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-let apiKey = "349d90e7a285f14e8d2236c5925edbde";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
-
 function showWeather(response) {
   console.log(response.data);
+
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let tempElement = document.querySelector("#temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
+  windElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
 }
+
+let apiKey = "349d90e7a285f14e8d2236c5925edbde";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showWeather);
